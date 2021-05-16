08f85f69b3fa64eb213b297c101d7fdeec33b9e2
mutissimo <mutissimo@gmail.com>
2021-05-16 22:36:24 +0300

    Создание линий

--- Program.cs
+++ Program.cs
@@ -1,56 +1,24 @@
 ﻿using System;
 using System.Collections.Generic;
 
+
 namespace Snake
 {
     class Program
     {
-		static void Main(string[] args)
-		{
+        static void Main(string[] args)
+        {
 			Point p1 = new Point(1, 3, '*');
 			p1.Draw();
 
 			Point p2 = new Point(4, 5, '#');
 			p2.Draw();
 
-			Point p3 = new Point(3, 2, '*');
-			p3.Draw();
+			HorizontalLine line = new HorizontalLine(5, 10, 8, '+');
+            line.Drow();
 
-			Point p4 = new Point(5, 4, '#');
-			p4.Draw();
-
-			Point p5 = new Point(2, 1, '*');
-			p5.Draw();
-
-			Point p6 = new Point(6, 6, '#');
-			p6.Draw();
-
-
-
-
-			List<int> numList = new List<int>();
-			numList.Add(0);
-			numList.Add(1);
-			numList.Add(2);
-
-			int x = numList[0];
-			int y = numList[1];
-			int z = numList[2];
-
-			foreach(int i in numList)
-            {
-				Console.WriteLine(i);
-            }
-
-			numList.Remove(0);
-
-			List<Point> pList = new List<Point>();
-			pList.Add(p1);
-			pList.Add(p2);
-			pList.Add(p3);
-			pList.Add(p4);
-			pList.Add(p5);
-			pList.Add(p6);
+			VerticalLine vline = new VerticalLine(5, 10, 1, '+');
+            vline.Drow();
 
 			Console.ReadLine();
 		}
